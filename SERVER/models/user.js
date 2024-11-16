3c657592433a6592d3bccaf9fccc2b24d5137f4b
manoj <manoj@illimitable.in>
2024-11-16 20:12:21 +0530

    update

--- SERVER/models/user.js
+++ SERVER/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     user_type: {type: 'string', required: true},
     teacher_code: {type: 'string'},
     employee_id: {type: 'string'},
+    specialization: {type: 'string'},
     department: {type: 'ObjectId'},
     registration_year: {type: 'number'},
     registration_number: {type: 'string'},
