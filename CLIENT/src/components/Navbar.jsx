ab9f510851118f21fcd4bcea4645fd119757582e
manoj <manoj@illimitable.in>
2024-12-05 10:15:16 +0530

    wip

--- CLIENT/src/components/Navbar.jsx
+++ CLIENT/src/components/Navbar.jsx
@@ -43,8 +43,8 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg fixed-top">
       <div className="container-fluid">
-        <div className="navbar-brand">
-          <Link className="nav-link active" to="/"> <img style={{maxWidth: "2rem", margin: "0rem 0.4rem 0rem 1rem"}} src="../src/assets/images/eduInsights-logo.png" alt="" /> EduInsights</Link>
+        <div className="navbar-brand" style={{padding: "0"}}>
+          <Link className="nav-link active" to="/"> <img style={{maxWidth: "3rem", margin: "0rem -0.5rem 0rem 0rem"}} src="../src/assets/images/eduInsights-logo-2.png" alt="" /> EduInsights</Link>
         </div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
